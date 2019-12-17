bd9f34b7fe56e227c36058fadf6525e5da51c31a
vietDuc <phanvietduc1@gmail.com>
2019-12-17 16:40:40 +0700

    tao thanh chan va chuyen dong nhap tu ban phim

--- game.js
+++ game.js
@@ -8,14 +8,62 @@ var ball = {
     dy: 2,
     radius: 20,
 }
+var paddle = {
+    x: 0,
+    y: canvas.height - 10,
+    width: 70,
+    height: 10,
+    speed: 5,
+
+    isMovingLeft: false,
+    isMovingRight: false,
+}
+
+document.addEventListener('keyup', function (event) {
+    console.log('KEY UP');
+    console.log(event);
+    if (event.keyCode == 39) {
+        paddle.isMovingLeft = false;
+    }
+    if (event.keyCode == 37) {
+        paddle.isMovingRight = false;
+    }
+});
+document.addEventListener('keydown', function (event) {
+    console.log('KEY DOWN');
+    console.log(event);
+    if (event.keyCode == 39) {
+        paddle.isMovingLeft = true;
+    }
+    if (event.keyCode == 37) {
+        paddle.isMovingRight = true;
+    }
+});
+
+function updatePaddlePosition() {
+    if (paddle.isMovingRight == true) {
+        paddle.x -= paddle.speed;
+    }; 
+    if (paddle.isMovingLeft == true) {
+        paddle.x += paddle.speed;
+    };
+}
 
 function drawBall() {
     context.beginPath();
-    context.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
     context.fillStyle = 'red';
     context.fill();
     context.closePath();
-}   
+}
+
+function drawPaddle() {
+    context.beginPath();
+    context.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    context.fillStyle = 'red';
+    context.fill();
+    context.closePath();
+}
 
 function handleBallCollideBounds() {
     if (ball.x < ball.radius || ball.x > canvas.width - ball.radius) {
@@ -34,9 +82,11 @@ function updateBallPosition() {
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
+    updatePaddlePosition();
     handleBallCollideBounds();
-    updateBallPosition(); 
+    updateBallPosition();
 
     requestAnimationFrame(draw);
 }
